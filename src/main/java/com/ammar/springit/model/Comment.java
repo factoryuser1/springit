bfaa02ec0da7bfd537cfa66bbcce0c3982d58644
factoryuser1 <factoryuser1@yahoo.com>
2021-09-08 08:36:42 -0700

    Initial Code

---
+++ src/main/java/com/ammar/springit/model/Comment.java
@@ -0,0 +1,50 @@
+package com.ammar.springit.model;
+
+import javax.persistence.Entity;
+import java.util.Objects;
+
+@Entity
+public class Comment {
+    private Long id;
+    private String body;
+
+    //tie back to Link
+    public Comment(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", body='" + body + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(id, comment.id) && Objects.equals(body, comment.body);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, body);
+    }
+}
